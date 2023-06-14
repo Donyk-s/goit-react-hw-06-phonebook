acca06c18206cbeb56c832c78886dd8386e2048f
Sergey Donyk <doncarlos@i.ua>
2023-06-14 03:29:18 +0300

    cdcd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,3 @@
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { Form } from './forma/Forma';
-// import ContactList from './contacklisst/Contactlist';
-// import Filter from './filter/Filter';
-// import css from '../components/App.module.css';
-
-// export const App = () => {
-//   const myContacts = useSelector(state => state.contacts.contacts);
-
-//   return (
-//     <div className={css.baseStyle}>
-//       <Form />
-//       <ContactList contacts={myContacts} />
-      
-//       <Filter />
-//     </div>
-//   );
-// };
-// App.jsx
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Form } from './forma/Forma';
--- src/components/contacklisst/Contactlist.js
+++ src/components/contacklisst/Contactlist.js
@@ -1,48 +1,11 @@
-// import { useDispatch, useSelector } from 'react-redux';
-// import { delContacts } from '../../redux/contactsSlice/ContactsSlice';
-// import css from './Contactlist.module.css';
-
-// const ContactList = () => {
-//   const contacts = useSelector(state => {
-//     const filter = state.filter.filter; // Отримуємо значення фільтра зі стану
-//     if (filter) {
-//       return state.contacts.contacts.filter(
-//         contact => contact.name.toLowerCase().includes(filter.toLowerCase())
-//       );
-//     }
-//     return state.contacts.contacts;
-//   });
-
-//   const dispatch = useDispatch();
-//   const handleDeleteContact = id => {
-//     dispatch(delContacts(id));
-//   };
-
-//   return (
-//     <ul className={`${css.contList} ${contacts.length ? '' : css.emptyList}`}>
-//       {contacts.map(({ id, name, number }) => (
-//         <li key={id} className={css.listItem}>
-//           <p>Name: {name}</p>
-//           <p>Number: {number}</p>
-//           <button className={css.myButton} onClick={() => handleDeleteContact(id)}>
-//             Delete
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default ContactList;
-
-
 import { useDispatch, useSelector } from 'react-redux';
 import { delContacts } from '../../redux/contactsSlice/ContactsSlice';
 import css from './Contactlist.module.css';
 
 const ContactList = () => {
   const contacts = useSelector(state => {
-    const filter = state.filter; // Отримуємо значення фільтра зі стану
+    const filter = state.filter; 
+    
     if (filter) {
       return state.contacts.contacts.filter(
         contact => contact.name.toLowerCase().includes(filter.toLowerCase())
